6e9bd5415ebd6b0c2e88dd7c9f5018e72ce84297
luishenriqueom <luis.henryke1@gmail.com>
2024-07-23 04:28:47 -0300

    fix(build)

--- frontend/src/app/(candidato)/meus_curriculos/page.tsx
+++ frontend/src/app/(candidato)/meus_curriculos/page.tsx
@@ -1,1 +1,7 @@
-  
+  export default function MeusCurriculos() {
+    return (
+        <>
+            <h1>Meus Curriculos</h1>
+        </>
+    );
+  }
---
+++ frontend/src/app/(candidato)/perfil/page.tsx
@@ -0,0 +1,7 @@
+export default function MeusCurriculos() {
+    return (
+        <>
+            <h1>Perfil do candidato</h1>
+        </>
+    );
+  }
